ce2625642ba5458e918b1c253c38566b4ea6187b
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-08-22 11:16:54 -0400

    Added the Layout component. Added the hoc Auxiliary. Mod index.htm and the App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 // import './App.css';
+import Layout from './components/Layout/Layout';
 
-function App() {
-    return (
-        // <div className="App">
-        <div>
 
-        </div>
-    );
+class App extends Component {
+
+    render() {
+        return (
+            // <div>
+            <Layout>c
+
+            </Layout>
+            // </div>
+        );
+    }
 }
 
 export default App;
