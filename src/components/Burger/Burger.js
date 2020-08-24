d74d80bdb907aab02c3a1ad74066b5957cb88ae8
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-08-23 22:46:18 -0400

    Mod BurgerBuilder container, by defining the ingredients state and rendering the Burger and passing the ingredients as props. Mod the Burger componenent, by adding iteration over the props and dinamically rendering the provided ingredients inside the props

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,21 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from './Burger.module.scss';
 
 const burger = (props) => {
+    const ingredients_array = Object.keys(props.ingredients)
+        .map(ingredient_key => {
+            return [...Array(props.ingredients[ingredient_key])].map((_, i) => {
+                return <BurgerIngredient key={ingredient_key + i} type={ingredient_key}/>
+            });
+        });
+
+    console.log(ingredients_array);
+    console.log(ingredients_array.flat(1));
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type={'bread-top'}/>
-            <BurgerIngredient type={'cheese'}/>
-            <BurgerIngredient type={'meat'}/>
+            {/*{ingredients_array}*/}
+            {ingredients_array.flat(1)}
             <BurgerIngredient type={'bread-bottom'}/>
-
         </div>
     );
 };
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,26 @@ import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger.js';
 
 class BurgerBuilder extends Component {
+    // The usual way:
+    // constructor() {
+    //     super();
+    //     this.state = {}
+    // }
+
+    // More modern way to define the state on a class based container (stateful component):
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
     render() {
         return (
             <Auxiliary>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>BurgerBuilder: Build controls. Area to add and remove ingredients</div>
             </Auxiliary>
         );
