75c20967caae2e9cf8d0251a0c819f4070c614e0
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-09-01 00:43:33 -0400

    Mod the Button UI component by adding imported instance of the classname package and used on the styling of the inner button (Other way of using multiple classnames with CSS Modules in React (classnames package))

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,13 @@
 import React from "react";
 import classes from './Button.module.scss';
+import cx from 'classnames';
 
 const button = (props) => (
     <button
         onClick={props.clicked}
-        className={[classes.Button, classes[props.buttonType]].join(' ')}>
+        // className={[classes.Button, classes[props.buttonType]].join(' ')}>
+        // Other way of using multiple classnames with CSS Modules in React (classnames package)
+        className={cx(classes.Button, classes[props.buttonType])}>
         {props.children}
     </button>
 );
