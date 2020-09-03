ac11019c496ec20f828743c9afcf156e20d9d6bc
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-09-03 17:12:59 -0400

    Added the NavigationItems and NivigationItem components, with its styling scss files. Imported the new NavigationItems component inside the Toolbar component

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from './NavigationItems.module.scss';
+
+import NavigationItem from "./NavigationItem/NavigationItem";
+
+const navigationItems = (props) => (
+    <ul className={classes.NavigationItems}>
+        <NavigationItem href={"/"} active={true}>Burger Builder</NavigationItem>
+        <NavigationItem href={"/"}>Checkout</NavigationItem>
+    </ul>
+);
+
+export default navigationItems;
