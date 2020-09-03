858497e38a161e5ada66cc89034e9f4d2fb712d3
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-09-03 18:07:36 -0400

    Added the SideDrawer component with its styping scss file. Imported the SideDrawer component inside the Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 import Auxiliary from './../../hoc/Auxiliary';
 import classes from './Layout.module.scss';
@@ -7,7 +8,7 @@ import classes from './Layout.module.scss';
 const layout = (props) => (
     <Auxiliary>
         <Toolbar/>
-        {/*<div>Layout: SideBar </div>*/}
+        <SideDrawer/>
         <main className={classes.Content}>
             {props.children}
         </main>
