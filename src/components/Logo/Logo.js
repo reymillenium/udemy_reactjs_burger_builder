dda804269111100b91fd8378931e046aff8aeb10
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-09-03 00:16:06 -0400

    Added logo image to images inside assets. Added the Loco component with its scss file. Added new Logo component into the Toolbar

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+import burgerLogo from './../../../src/assets/images/burger_only_centered_trimmed_logo.png';
+import classes from './Logo.module.scss';
+
+const logo = (props) => (
+    <div className={classes.Logo}>
+        <img src={burgerLogo} alt="BurgerBuilder"/>
+    </div>
+);
+
+export default logo;
