b5f3b8832331dfa5cb97cfd4762875e9c7a1df65
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-09-05 19:25:18 -0400

    Added top 0 to SideDrawer class on its styling file, in order to make it fit with uper border of the page. Modified the SideDrawer component, by adding a BackDrop and hiding it depending of the props received. Mod Layout component by adding logic to how the SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, {Component} from "react";
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 import Auxiliary from './../../hoc/Auxiliary';
 import classes from './Layout.module.scss';
 
-const layout = (props) => (
-    <Auxiliary>
-        <Toolbar/>
-        <SideDrawer/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Auxiliary>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
 
-export default layout;
+    sideDrawerCloseHandler = () => {
+        this.setState(
+            {
+                showSideDrawer: false
+            }
+        );
+    }
+
+    render() {
+        return (
+            <Auxiliary>
+                <Toolbar/>
+                <SideDrawer
+                    show={this.state.showSideDrawer}
+                    close={this.sideDrawerCloseHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Auxiliary>
+        );
+    }
+
+}
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,22 +1,29 @@
 import React from "react";
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import BackDrop from '../../UI/Backdrop/Backdrop';
+import Auxiliary from '../../../hoc/Auxiliary';
 
 import classes from './SideDrawer.module.scss';
 
 const sideDrawer = (props) => {
-    //...
+    let attachedClasses = [classes.SideDrawer, props.show ? classes.Open : classes.Close].join(' ');
+
     return (
-        <div className={classes.SideDrawer}>
-            {/*<Logo height={"11%"}/>*/}
-            {/* Another way without using props: */}
-            <div className={classes.Logo}>
-                <Logo/>
+        <Auxiliary>
+            <BackDrop show={props.show}
+                      backdrop_click={props.close}/>
+            <div className={attachedClasses}>
+                {/*<Logo height={"11%"}/>*/}
+                {/* Another way without using props: */}
+                <div className={classes.Logo}>
+                    <Logo/>
+                </div>
+                <nav>
+                    <NavigationItems/>
+                </nav>
             </div>
-            <nav>
-                <NavigationItems/>
-            </nav>
-        </div>
+        </Auxiliary>
     );
 
 };
