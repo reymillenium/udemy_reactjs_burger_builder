8a7158e33e8daf2fbdb79daaecdfd0882a9fb1b6
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-09-07 07:13:18 -0400

    Added PropTypes validation for the Backdrop component

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 import classes from './backdrop.module.scss';
 
@@ -6,5 +7,10 @@ const backdrop = (props) => (
     props.show ?
         <div className={classes.Backdrop} onClick={props.backdrop_click}></div> : null
 );
+
+backdrop.propTypes = {
+    show: PropTypes.bool.isRequired,
+    backdrop_click: PropTypes.func.isRequired,
+}
 
 export default backdrop;
