7e0e26876b82675bb866408003074ae7dfb7cd1b
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-09-07 07:28:52 -0400

    Added PropTypes validation for the Logo component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,16 @@
 import React from "react";
 import burgerLogo from './../../../src/assets/images/burger_only_centered_trimmed_logo.png';
 import classes from './Logo.module.scss';
+import PropTypes from 'prop-types';
 
 const logo = (props) => (
     <div className={classes.Logo} style={{height: props.height}}>
         <img src={burgerLogo} alt="BurgerBuilder"/>
     </div>
 );
+
+logo.propTypes = {
+    height: PropTypes.number
+}
 
 export default logo;
