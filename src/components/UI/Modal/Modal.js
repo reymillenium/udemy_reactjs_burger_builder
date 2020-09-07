c6f20f1413892becb25f68ee69c20c7a60c44173
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-09-07 07:10:42 -0400

    Added PropTypes validation for the Modal component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Auxiliary from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
+import PropTypes from 'prop-types';
 
 import classes from './Modal.module.scss';
 
@@ -21,4 +22,8 @@ const modal = (props) => (
     </Auxiliary>
 );
 
+modal.propTypes = {
+    modalClosed: PropTypes.func.isRequired,
+    show: PropTypes.bool.isRequired
+}
 export default modal;
