fed9c01344361ce4177afb93fea4d3a66b7fb34b
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-09-07 21:19:44 -0400

    Transformed the OrderSummary and Modal func based components, into class based components. Added temporaly the 'componentWillUpdate' and 'componentDidUpdate' methods on both components

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,38 +1,48 @@
-import React from "react";
+import React, {Component} from "react";
 import Auxiliary from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
 import PropTypes from 'prop-types';
 
-const orderSummary = (props) => {
-    const ingredientsSummaryList = Object.keys(props.ingredients).map(igKey => {
-        return (
-            <li key={igKey}>
-                {/*<li>{igKey.charAt(0).toUpperCase() + igKey.slice(1)}: {props.ingredients[igKey]}</li>*/}
-                <span style={{textTransform: "capitalize"}}>{igKey}</span>: {props.ingredients[igKey]}
-            </li>
-        );
-    });
+class OrderSummary extends Component {
+    componentWillUpdate(nextProps, nextState, nextContext) {
+        console.log('[OrderSummary] -> componentWillUpdate');
+    }
 
-    return (
-        <Auxiliary>
-            <h3>Your Order</h3>
-            <p>Delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientsSummaryList}
-            </ul>
-            <p><strong>Total Price: {props.total_price} USD</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button buttonType={'Danger'} clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button buttonType={'Success'} clicked={props.purchaseContinue}>CONTINUE</Button>
-        </Auxiliary>
-    );
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('[OrderSummary] -> componentDidUpdate');
+    }
+
+    render() {
+        const ingredientsSummaryList = Object.keys(this.props.ingredients).map(igKey => {
+            return (
+                <li key={igKey}>
+                    {/*<li>{igKey.charAt(0).toUpperCase() + igKey.slice(1)}: {props.ingredients[igKey]}</li>*/}
+                    <span style={{textTransform: "capitalize"}}>{igKey}</span>: {this.props.ingredients[igKey]}
+                </li>
+            );
+        });
+
+        return (
+            <Auxiliary>
+                <h3>Your Order</h3>
+                <p>Delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientsSummaryList}
+                </ul>
+                <p><strong>Total Price: {this.props.total_price} USD</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button buttonType={'Danger'} clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button buttonType={'Success'} clicked={this.props.purchaseContinue}>CONTINUE</Button>
+            </Auxiliary>
+        );
+    }
 }
 
-orderSummary.propTypes = {
+OrderSummary.propTypes = {
     ingredients: PropTypes.object.isRequired,
     total_price: PropTypes.number.isRequired,
     purchaseCanceled: PropTypes.func.isRequired,
     purchaseContinue: PropTypes.func.isRequired
 }
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,29 +1,41 @@
-import React from "react";
+import React, {Component} from "react";
 import Auxiliary from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 import PropTypes from 'prop-types';
 
 import classes from './Modal.module.scss';
 
-const modal = (props) => (
-    <Auxiliary>
-        <Backdrop
-            show={props.show}
-            backdrop_click={props.modalClosed}/>
+class Modal extends Component {
+    componentWillUpdate(nextProps, nextState, nextContext) {
+        console.log('[Modal] -> componentWillUpdate');
+    }
 
-        <div className={classes.Modal}
-             style={{
-                 transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                 opacity: props.show ? '1' : '0'
-             }}
-        >
-            {props.children}
-        </div>
-    </Auxiliary>
-);
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('[Modal] -> componentDidUpdate');
+    }
 
-modal.propTypes = {
+    render() {
+        return (
+            <Auxiliary>
+                <Backdrop
+                    show={this.props.show}
+                    backdrop_click={this.props.modalClosed}/>
+
+                <div className={classes.Modal}
+                     style={{
+                         transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                         opacity: this.props.show ? '1' : '0'
+                     }}
+                >
+                    {this.props.children}
+                </div>
+            </Auxiliary>
+        );
+    }
+}
+
+Modal.propTypes = {
     modalClosed: PropTypes.func.isRequired,
     show: PropTypes.bool.isRequired
 }
-export default modal;
+export default Modal;
