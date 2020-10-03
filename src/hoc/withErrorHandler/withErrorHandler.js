9dccc8f9b3c4715b624730312493ce0354de4572
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-10-03 00:01:11 -0400

    Added the hoc withErrorHandler

---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Modal from '../../components/UI/Modal/Modal';
+import Auxiliary from '../Auxiliary/Auxiliary';
+
+const withErrorHandler = (WrappedComponent) => {
+    return (props) => {
+        return (
+            <Auxiliary>
+                <Modal>
+                    Something didn't work!
+                </Modal>
+                <WrappedComponent {...props}/>
+            </Auxiliary>
+        );
+    }
+}
+
+export default withErrorHandler;
