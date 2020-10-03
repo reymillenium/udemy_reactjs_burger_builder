033f5e3a32535c104c0aa7b04bc5c2a50df5c7c6
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-10-03 00:52:36 -0400

    Modified the withErrorHandler hoc, by changing it into a class based component (class factory, nameless), whcih includes a Modal with the axios error and the wrapped component. Used withErrorHandler by wrapping BurgerBuilder with it

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,17 +1,49 @@
-import React from "react";
+import React, {Component} from "react";
 import Modal from '../../components/UI/Modal/Modal';
 import Auxiliary from '../Auxiliary/Auxiliary';
 
-const withErrorHandler = (WrappedComponent) => {
-    return (props) => {
-        return (
-            <Auxiliary>
-                <Modal>
-                    Something didn't work!
-                </Modal>
-                <WrappedComponent {...props}/>
-            </Auxiliary>
-        );
+const withErrorHandler = (WrappedComponent, axios) => {
+    return class extends Component {
+        state = {
+            error: null
+        }
+
+        componentDidMount() {
+            // Clears the error on the request
+            axios.interceptors.request.use(request => {
+                this.setState({
+                    error: null
+                });
+                return request;
+            });
+
+            axios.interceptors.response.use(response => response, error => {
+                this.setState({
+                    error: error
+                });
+            });
+        }
+
+        errorConfirmedHandler = () => {
+            this.setState({
+                error: null
+            });
+        }
+
+        render() {
+            return (
+                <Auxiliary>
+                    <Modal
+                        show={this.state.error}
+                        modalClosed={this.errorConfirmedHandler}>
+                        {/*Something didn't work!*/}
+                        {this.state.error ? this.state.error.message : null}
+                        {/*{this.state.error.message}*/}
+                    </Modal>
+                    <WrappedComponent {...this.props}/>
+                </Auxiliary>
+            );
+        }
     }
 }
 
