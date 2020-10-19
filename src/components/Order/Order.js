edb43fd04cb0115ec6a6f6f59b0c4e6c18749e22
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-10-19 07:48:31 -0400

    Implemented another version of retrieving the ingredients from props and transforming them into a string, withput having to access directly to each one of them

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,23 @@
 import React from "react";
 import classes from './Order.module.scss';
+import BurgerIngredient from "../Burger/BurgerIngredient/BurgerIngredient";
 
 const order = (props) => {
+    const capitalize = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+    }
+
+    const ingredientsArray = [];
+    for (let ingredientName in props.ingredients) {
+        ingredientsArray.push(`${capitalize(ingredientName)} (${props.ingredients[ingredientName]})`);
+    }
+    let ingredientsString = ingredientsArray.join(', ');
+
     return (
         <div className={classes.Order}>
             <p>
-                Ingredients: Salad ({props.ingredients.salad}), Bacon ({props.ingredients.bacon}), Cheese ({props.ingredients.cheese}), Meat ({props.ingredients.meat})
+                {/*Ingredients: Salad ({props.ingredients.salad}), Bacon ({props.ingredients.bacon}), Cheese ({props.ingredients.cheese}), Meat ({props.ingredients.meat})*/}
+                Ingredients: {ingredientsString}
             </p>
             <p>
                 Price: <strong>{props.price} USD</strong>
