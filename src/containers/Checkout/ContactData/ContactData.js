7c163780514b359221642f3c751235eaf4b01a1b
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-10-19 02:36:02 -0400

    Added conditional rendering of the form. Is loading is true then it renders the spinner instead

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 
 import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.module.scss';
 import axios from '../../../axios-orders';
 
@@ -55,17 +56,24 @@ class ContactData extends Component {
     }
 
     render() {
+        let form = (
+            <form action="">
+                <input className={classes.Input} type="text" name={'name'} placeholder={'Your Name'}/>
+                <input className={classes.Input} type="email" name={'email'} placeholder={'Your Email'}/>
+                <input className={classes.Input} type="text" name={'street'} placeholder={'Your Street'}/>
+                <input className={classes.Input} type="text" name={'postal'} placeholder={'Your Postal Code'}/>
+
+                <Button buttonType={'Success'} clicked={this.orderHandler}>ORDER</Button>
+            </form>
+        );
+        if (this.state.loading) {
+            form = <Spinner/>;
+        }
+
         return (
             <div className={classes.ContactData}>
                 <h4>Enter your Contact Data</h4>
-                <form action="">
-                    <input className={classes.Input} type="text" name={'name'} placeholder={'Your Name'}/>
-                    <input className={classes.Input} type="email" name={'email'} placeholder={'Your Email'}/>
-                    <input className={classes.Input} type="text" name={'street'} placeholder={'Your Street'}/>
-                    <input className={classes.Input} type="text" name={'postal'} placeholder={'Your Postal Code'}/>
-
-                    <Button buttonType={'Success'} clicked={this.orderHandler}>ORDER</Button>
-                </form>
+                {form}
             </div>
         );
     }
