efeab918819bfa8031fab0b5babd3ab560410452
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-10-19 06:48:46 -0400

    Modified the Orders container by using axios to retrieve the Orders from FireBase. Added componentDidMount to fetch the Orders. Modified its scss file by reducing the width from 100% to 80%

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,7 +1,42 @@
 import React, {Component} from "react";
 import Order from '../../components/Order/Order'
+import axios from '../../axios-orders';
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(response => {
+                const fetchedOrders = [];
+
+                for (let key in response.data) {
+                    fetchedOrders.push({
+                        // Gets all the fields in the js object and then adds a new one (id)
+                        ...response.data[key],
+                        id: key
+                    });
+                }
+
+                // console.log('Orders -> fetchedOrders = ', fetchedOrders);
+
+                this.setState({
+                    loading: false,
+                    orders: fetchedOrders
+                });
+            })
+            .catch(error => {
+                    this.setState({
+                        loading: false
+                    });
+                }
+            );
+    }
+
     render() {
         return (
             <div>
@@ -12,4 +47,4 @@ class Orders extends Component {
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
