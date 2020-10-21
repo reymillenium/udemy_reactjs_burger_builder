742263b41387a059f9fa64d05c0ce553b322bd95
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-10-21 02:28:05 -0400

    Modified the Input component, by adding the onChange event to each type of input, received from props

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -7,18 +7,23 @@ const input = (props) => {
         case ('input'):
             inputElement = <input
                 className={classes.InputElement}
-                {...props.elementConfig} defaultValue={props.value}/>;
+                {...props.elementConfig}
+                defaultValue={props.value}
+                onChange={props.changed}/>;
             break;
         case ('textarea'):
             inputElement = <textarea
                 className={classes.InputElement}
-                {...props.elementConfig} defaultValue={props.value}/>;
+                {...props.elementConfig}
+                defaultValue={props.value}
+                onChange={props.changed}/>;
             break;
         case ('select'):
             inputElement = (
                 <select
                     className={classes.InputElement}
                     defaultValue={props.value}>
+                    onChange={props.changed}
                     {props.elementConfig.options.map(option => (
                         <option key={option.value} value={option.value}>
                             {option.displayValue}
@@ -30,7 +35,8 @@ const input = (props) => {
         default:
             inputElement = <input
                 className={classes.InputElement}
-                {...props.elementConfig} value={props.value}/>;
+                {...props.elementConfig} value={props.value}
+                onChange={props.changed}/>;
             break;
     }
 
