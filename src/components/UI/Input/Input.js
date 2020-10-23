3f62be6dc95d0d594c21c1ad51cc69573af84e20
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-10-22 23:45:14 -0400

    Modified the Input component in order to receive the invalid property, inetad of the valid. Removed validationRules from the select box of the state on ContactData. Included also a shouldValidate property on the props passed down into the Input component, from the ContactData container

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -5,7 +5,7 @@ const input = (props) => {
     let inputElement = null;
     const inputClasses = [classes.InputElement];
 
-    if (!props.valid) {
+    if (props.invalid && props.shouldValidate) {
         inputClasses.push(classes.Invalid);
     }
 
