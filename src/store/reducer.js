d9168af229b7a4a9d5988e2a37e502d27f731998
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-10-28 23:59:54 -0400

    Mods on the reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,56 +1,73 @@
 import * as actionTypes from './actions';
 
+const INGREDIENT_PRICES = {
+    salad: 0.50,
+    cheese: 0.40,
+    meat: 1.30,
+    bacon: 0.70
+};
+
 const initialState = {
     ingredients: null,
-    totalPrice: 4
+    totalPrice: 4,
+    purchasable: false,
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.ADD_INGREDIENT:
-            const newPerson = {
-                id: (new Date() + Math.random()),
-                name: action.payload.name,
-                age: action.payload.age
-            }
+            const oldCount = state.ingredients[action.payload.type];
+            const updateCount = oldCount + 1;
+            const updatedIngredients = {
+                ...state.ingredients
+            };
+            updatedIngredients[action.payload.type] = updateCount;
+            // this.state.ingredients[type] += 1;
 
+            const priceAddition = INGREDIENT_PRICES[action.payload.type];
+            const oldPrice = state.totalPrice;
+            const newPrice = (parseFloat(oldPrice) + parseFloat(priceAddition)).toFixed(2);
 
-
-
-
-            // const oldCount = this.state.ingredients[type];
-            // const updateCount = oldCount + 1;
-            // const updatedIngredients = {
-            //     ...this.state.ingredients
-            // };
-            // updatedIngredients[type] = updateCount;
-            // // this.state.ingredients[type] += 1;
-            //
-            //
-            // const priceAddition = INGREDIENT_PRICES[type];
-            // const oldPrice = this.state.totalPrice;
-            // const newPrice = (parseFloat(oldPrice) + parseFloat(priceAddition)).toFixed(2);
-            //
-            // this.setState({
-            //     ingredients: updatedIngredients,
-            //     totalPrice: newPrice
-            // });
-            //
             // this.updatePurchasableState(updatedIngredients);
 
-
-
-
-
             return {
                 ...state,
-                persons: state.persons.concat(newPerson)
+                ingredients: updatedIngredients,
+                totalPrice: newPrice,
+                purchasable: updatePurchasableState(updatedIngredients)
+
             };
         case actionTypes.REMOVE_INGREDIENT:
-            const updatedArray = state.persons.filter(person => person.id !== action.payload.id);
+            // const updatedArray = state.persons.filter(person => person.id !== action.payload.id);
+            // return {
+            //     ...state,
+            //     persons: updatedArray
+            // };
+
+
+
+
+            const oldCount2 = state.ingredients[action.payload.type];
+
+            if (oldCount2 <= 0) {
+                return;
+            }
+            const updateCount2 = oldCount2 - 1;
+            const updatedIngredients2 = {
+                ...state.ingredients
+            };
+            updatedIngredients2[action.payload.type] = updateCount2;
+
+            const priceReduction2 = INGREDIENT_PRICES[action.payload.type];
+            const oldPrice2 = state.totalPrice;
+            const newPrice2 = (parseFloat(oldPrice2) - parseFloat(priceReduction2)).toFixed(2);
+
             return {
                 ...state,
-                persons: updatedArray
+                ingredients: updatedIngredients2,
+                totalPrice: newPrice2,
+                purchasable: updatePurchasableState(updatedIngredients2)
+
             };
         default:
             return {
@@ -59,6 +76,10 @@ const reducer = (state = initialState, action) => {
     }
 
     // return state;
+}
+
+const updatePurchasableState = (currentIngredients) => {
+    return Object.values(currentIngredients).some(el => el > 0);
 }
 
 export default reducer;
