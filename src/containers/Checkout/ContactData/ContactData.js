e4bb0abaaf16926c422b826f3ac7dce723bd928c
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-11-30 21:03:23 -0500

    Modified the ContactDats container by commenting the axios related code in the method orderHandler

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -130,13 +130,13 @@ class ContactData extends Component {
             orderData: formData
         }
 
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading: false});
-                this.props.history.push('/');
-            }).catch(error => {
-            this.setState({loading: false});
-        });
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({loading: false});
+        //         this.props.history.push('/');
+        //     }).catch(error => {
+        //     this.setState({loading: false});
+        // });
 
         // this.props.history.push("/checkout");
     }
