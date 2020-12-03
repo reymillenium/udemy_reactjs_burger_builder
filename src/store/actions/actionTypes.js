308c107e7a6c4870cc3e3a5ea28b8d72e5d1bdf0
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-12-03 01:29:12 -0500

    Added the action types: FETCH_ORDERS_START, FETCH_ORDERS_SUCCESS & FETCH_ORDERS_FAIL to the actionTypes.js file

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -9,3 +9,7 @@ export const PURCHASE_BURGER_FAIL = 'PURCHASE_BURGER_FAIL';
 
 // Will be dispatched whenever we load the Checkout page:
 export const PURCHASE_INIT = 'PURCHASE_INIT';
+
+export const FETCH_ORDERS_START = 'FETCH_ORDERS_START';
+export const FETCH_ORDERS_SUCCESS = 'FETCH_ORDERS_SUCCESS';
+export const FETCH_ORDERS_FAIL = 'FETCH_ORDERS_FAIL';
