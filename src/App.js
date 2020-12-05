243727f2f930dda966f50a1482c456a393feb889
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-12-05 00:25:01 -0500

    Added a route for the login at 'src/App.js'

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
 import Orders from "./containers/Orders/Orders";
+import Auth from "./containers/Auth/Auth";
 
 class App extends Component {
 
@@ -32,6 +33,7 @@ class App extends Component {
                         {/* Gets the first match only. The order matters. So, the root must be the last one */}
                         <Route path={"/checkout"} component={Checkout}/>
                         <Route path={"/orders"} component={Orders}/>
+                        <Route path={"/login"} component={Auth}/>
                         <Route path={"/"} component={BurgerBuilder}/>
                     </Switch>
                 </Layout>
