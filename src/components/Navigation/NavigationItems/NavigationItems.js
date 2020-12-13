c70c4a9b0c94ffc27b36ef2ad0b9574f29f0794f
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-12-13 00:50:41 -0500

    Modified the NavigationItems component, by conditionally rendering the logout and login NavigationItem, as well as the orders NavigationItem, based on the received boolean isAuthenticated, from props.

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,8 +6,14 @@ import NavigationItem from "./NavigationItem/NavigationItem";
 const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem href={"/"} exact={true}>Burger Builder</NavigationItem>
-        <NavigationItem href={"/orders"}>Orders</NavigationItem>
-        <NavigationItem href={"/login"}>Login</NavigationItem>
+
+        {props.isAuthenticated
+            ? <NavigationItem href={"/orders"}>Orders</NavigationItem>
+            : null}
+
+        {props.isAuthenticated
+            ? <NavigationItem href={"/logout"}>Logout</NavigationItem>
+            : <NavigationItem href={"/login"}>Login</NavigationItem>}
     </ul>
 );
 
