a5b0b26232847f5457dd82cf314fe1a1d4420068
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-12-20 05:45:17 -0500

    Applied route protection on the front-end with guards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 // import './App.css';
-import {Route, Switch, withRouter} from 'react-router-dom';
+import {Route, Switch, withRouter, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import * as actionCreators from "./store/actions/index";
 
@@ -23,24 +23,59 @@ class App extends Component {
         let routes = (
             <Switch>
                 <Route path={"/auth"} component={Auth}/>
-                <Route path={"/"} component={BurgerBuilder}/>
+                <Route path={"/"} exact component={BurgerBuilder}/>
+                <Redirect to={"/"}/>
             </Switch>
         );
+
         if (this.props.isAuthenticated) {
             routes = (
                 <Switch>
                     <Route path={"/checkout"} component={Checkout}/>
                     <Route path={"/orders"} component={Orders}/>
-                    {/*<Route path={"/auth"} component={Auth}/>*/}
+                    <Route path={"/auth"} component={Auth}/>
                     <Route path={"/logout"} component={Logout}/>
-                    <Route path={"/"} component={BurgerBuilder}/>
+                    <Route path={"/"} exact component={BurgerBuilder}/>
+                    <Redirect to={"/"}/>
                 </Switch>
             );
         }
+
+
+        // let routes = null;
+        //
+        // if (this.props.isAuthenticated) {
+        //     routes = (
+        //         <Switch>
+        //             <Route path={"/checkout"} component={Checkout}/>
+        //             <Route path={"/orders"} component={Orders}/>
+        //             <Route path={"/auth"} component={Auth}/>
+        //             <Route path={"/logout"} component={Logout}/>
+        //             <Route path={"/"} component={BurgerBuilder}/>
+        //             <Redirect to={"/"}/>
+        //         </Switch>
+        //     );
+        // } else {
+        //     routes = (
+        //         <Switch>
+        //             <Route path={"/auth"} component={Auth}/>
+        //             <Route path={"/"} component={BurgerBuilder}/>
+        //             <Redirect to={"/"}/>
+        //         </Switch>
+        //     );
+        // }
+
         return (
             <Auxiliary>
                 <Layout>
                     {routes}
+                    {/*<Switch>*/}
+                    {/*    <Route path={"/checkout"} component={Checkout}/>*/}
+                    {/*    <Route path={"/orders"} component={Orders}/>*/}
+                    {/*    <Route path={"/auth"} component={Auth}/>*/}
+                    {/*    <Route path={"/logout"} component={Logout}/>*/}
+                    {/*    <Route path={"/"} component={BurgerBuilder}/>*/}
+                    {/*</Switch>*/}
                 </Layout>
             </Auxiliary>
         );
