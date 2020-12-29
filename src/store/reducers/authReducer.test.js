35db4fea3122aff7b6bc53b6e311cb09ac9d640d
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-12-29 10:29:26 -0500

    Added the test file src/store/reducers/authReducer.test.js with several jest tests to cover the authReducer

---
+++ src/store/reducers/authReducer.test.js
@@ -0,0 +1,34 @@
+import authReducer from "./authReducer";
+import * as actionTypes from '../actions/actionTypes';
+
+describe('authReducer', () => {
+    const initialState = {
+        idToken: null,
+        userId: null,
+        error: null,
+        loading: null,
+        authRedirectPath: '/'
+    };
+    it('should return the initial state when the state is just getting setup at the beginning of our app', () => {
+        expect(authReducer(undefined, {})).toEqual(initialState);
+    });
+
+    it('Should store the token upon login', () => {
+        const afterAuthSuccessState = {
+            idToken: 'some-token',
+            userId: 'some-user-id',
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        }
+
+        expect(authReducer(initialState, {
+            type: actionTypes.AUTH_SUCCESS,
+            payload: {
+                idToken: 'some-token',
+                userId: 'some-user-id'
+            }
+        })).toEqual(afterAuthSuccessState);
+    });
+
+});
